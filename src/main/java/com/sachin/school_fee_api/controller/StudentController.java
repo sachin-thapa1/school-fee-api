ac6e5daddd3a5a6b78c9b5f360bd00cceb5442ca
Sachin Thapa <sachinthapa.xiao12@gmail.com>
2026-01-04 23:07:17 +0545

    added functions like getAll, byid, update and delete

--- src/main/java/com/sachin/school_fee_api/controller/StudentController.java
+++ src/main/java/com/sachin/school_fee_api/controller/StudentController.java
@@ -1,8 +1,17 @@
 package com.sachin.school_fee_api.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sachin.school_fee_api.entity.Student;
@@ -23,5 +32,26 @@ public class StudentController {
         System.out.println(">>> returning student = "+ saved);
         return saved;
     }
+
+    @GetMapping
+    public List<Student> all(){
+        return service.getAllStudents();
+  }
+
+    @GetMapping("/{id}")
+     public Student one(@PathVariable Long id) {
+        return service.getStudentById(id);
+  }
+
+   @PutMapping("/{id}")
+   public Student replace(@PathVariable Long id, @RequestBody Student s){
+    return service.updateStudent(id, s);
+   }
+
+   @DeleteMapping ("/{id}")
+   @ResponseStatus (HttpStatus.NO_CONTENT)
+   public void delete(@PathVariable Long id){
+    service.deleteStudent(id);
+   }
 
 }
--- src/main/java/com/sachin/school_fee_api/services/StudentService.java
+++ src/main/java/com/sachin/school_fee_api/services/StudentService.java
@@ -1,6 +1,10 @@
 package com.sachin.school_fee_api.services;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.sachin.school_fee_api.entity.Student;
 import com.sachin.school_fee_api.repository.StudentRepository;
@@ -17,5 +21,20 @@ public class StudentService {
         System.out.println(">>> after save id = " + saved.getId());
         return saved;
     }
-
+    public List <Student>getAllStudents(){
+        return repo.findAll();
+    }
+    public Student getStudentById(Long id){
+        return repo.findById(id).
+        orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+    public void deleteStudent(Long id){
+        if(!repo.existsById(id)) throw new
+        ResponseStatusException(HttpStatus.NOT_FOUND);
+        repo.deleteById(id);
+    }
+    public Student updateStudent(Long id, Student incoming){
+        return repo.findById(id).map(s -> {s.setName(incoming.getName()); return repo.save(s);
+        }) . orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
 }
